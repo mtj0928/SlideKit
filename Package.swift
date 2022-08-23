8f4f0708392a4a1f5e2eb132134f6afce1051b57
mtj0928 <mtj0928@users.noreply.github.com>
2022-08-23 15:21:18 +0900

    make project

--- Package.swift
+++ Package.swift
@@ -6,23 +6,11 @@ import PackageDescription
 let package = Package(
     name: "SlideKit",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SlideKit",
-            targets: ["SlideKit"]),
+        .library(name: "SlideKit", targets: ["SlideKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SlideKit",
-            dependencies: []),
-        .testTarget(
-            name: "SlideKitTests",
-            dependencies: ["SlideKit"]),
+        .target(name: "SlideKit", dependencies: []),
+        .testTarget(name: "SlideKitTests", dependencies: ["SlideKit"]),
     ]
 )
