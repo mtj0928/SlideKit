4dec065f99b03b187f24a4cb4ce7ee7511157cd7
mtj0928 <mtj0928@users.noreply.github.com>
2022-08-24 08:22:08 +0900

    Add EnvironmentValues+ScreenSize.swift

--- Sources/SlideKit/EnvironmentVlaues/EnvironmentValues+ScreenSize.swift
+++ Sources/SlideKit/EnvironmentVlaues/EnvironmentValues+ScreenSize.swift
@@ -1,5 +1,5 @@
 //
-//  EnvironmentValues+SlideSize.swift
+//  EnvironmentValues+ScreenSize.swift
 //  
 //
 //  Created by Junnosuke Matsumoto on 2022/08/24.
@@ -8,11 +8,11 @@
 import SwiftUI
 
 private enum Key: EnvironmentKey {
-    static let defaultValue = CGSize(width: 1920, height: 1080)
+    static let defaultValue: CGSize = .zero
 }
 
 extension EnvironmentValues {
-    public var slideSize: CGSize {
+    public var screenSize: CGSize {
         get { self[Key.self] }
         set { self[Key.self] = newValue }
     }
