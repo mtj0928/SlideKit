8f8f3233de4c95ee5bc487d39060770bda5adada
mtj0928 <mtj0928@users.noreply.github.com>
2022-08-30 21:45:36 +0900

    remove SlideKitCLI

--- Package.swift
+++ Package.swift
@@ -7,30 +7,10 @@ let package = Package(
     name: "SlideKit",
     platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
-        .executable(name: "slidekit-generator", targets: ["SlideKitCLI"]),
         .library(name: "SlideKit", targets: ["SlideKit"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.4"),
-        .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.15.1"),
-        .package(url: "https://github.com/yonaskolb/XcodeGen.git", from: "2.32.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
-    ],
+    dependencies: [],
     targets: [
-        .executableTarget(
-            name: "SlideKitCLI",
-            dependencies: [
-                "SlideKit",
-                .product(name: "Stencil", package: "Stencil"),
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "XcodeGenKit", package: "XcodeGen"),
-                .product(name: "ProjectSpec", package: "XcodeGen"),
-                .product(name: "Logging", package: "swift-log"),
-            ],
-            resources: [
-                .process("Resources")
-            ]
-        ),
         .target(name: "SlideKit", dependencies: []),
         .testTarget(name: "SlideKitTests", dependencies: ["SlideKit"]),
     ]
