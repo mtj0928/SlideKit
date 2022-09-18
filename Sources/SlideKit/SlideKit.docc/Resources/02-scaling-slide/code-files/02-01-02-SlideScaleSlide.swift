316ab5cbeb09d7abf81ee50381c524b442b8d80f
mtj0928 <mtj0928@users.noreply.github.com>
2022-09-18 17:27:49 +0900

    Add a new tuorial "Scaling Slide"

---
+++ Sources/SlideKit/SlideKit.docc/Resources/02-scaling-slide/code-files/02-01-02-SlideScaleSlide.swift
@@ -0,0 +1,28 @@
+import SlideKit
+import SwiftUI
+
+struct SlideScaleSlide: Slide {
+    var body: some View {
+        HeaderSlide("SlideScale on SlideKit") {
+            Item("View on SlideKit is needed to be scaled to keep the ratio of layout and the slide.")
+
+            Item("This shape is not scaled.") {
+                VStack(spacing: 8) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.accentColor)
+                        .frame(width: 150, height: 150)
+                    Text("Non-scaled Shape")
+                        .font(.system(size: 30))
+                }
+            }
+        }
+    }
+}
+
+struct SlideScaleSlide_Previews: PreviewProvider {
+    static var previews: some View {
+        SlidePreview {
+            SlideScaleSlide()
+        }
+    }
+}
---
+++ Sources/SlideKit/SlideKit.docc/Resources/02-scaling-slide/code-files/02-01-04-SlideScaleSlide.swift
@@ -0,0 +1,42 @@
+import SlideKit
+import SwiftUI
+
+struct SlideScaleSlide: Slide {
+
+    @Environment(\.slideScale)
+    var slideScale: CGFloat
+
+    var body: some View {
+        HeaderSlide("SlideScale on SlideKit") {
+            Item("View on SlideKit is needed to be scaled to keep the ratio of layout and the slide.")
+
+            Item("This shape is scaled.") {
+                VStack(spacing: slideScale * 8) {
+                    RoundedRectangle(cornerRadius: slideScale * 10)
+                        .fill(Color.accentColor)
+                        .frame(width: slideScale * 150, height: slideScale * 150)
+                    Text("Scaled Shape")
+                        .font(.system(size: slideScale * 30))
+                }
+            }
+
+            Item("This shape is not scaled.") {
+                VStack(spacing: 8) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.accentColor)
+                        .frame(width: 150, height: 150)
+                    Text("Non-scaled Shape")
+                        .font(.system(size: 30))
+                }
+            }
+        }
+    }
+}
+
+struct SlideScaleSlide_Previews: PreviewProvider {
+    static var previews: some View {
+        SlidePreview {
+            SlideScaleSlide()
+        }
+    }
+}
