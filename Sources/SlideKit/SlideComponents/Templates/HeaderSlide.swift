fc154b554027c7603afb03e8beae90a0848fda24
mtj0928 <mtj0928@users.noreply.github.com>
2022-09-20 23:06:49 +0900

    add DocC to HeaderSlide

--- Sources/SlideKit/SlideComponents/Templates/HeaderSlide.swift
+++ Sources/SlideKit/SlideComponents/Templates/HeaderSlide.swift
@@ -7,13 +7,37 @@
 
 import SwiftUI
 
-public struct HeaderSlide: View {
+/// A template slide which has a title and a content.
+///
+/// ``HeaderSlide``  is one of template slides.
+/// The design can be customized by using ``HeaderSlideStyle``.
+///
+/// ```swift
+/// var body: some View {
+///     HeaderSlide("SampleSlide") {
+///         Item("1st item")
+///         Item("2nd item")
+///         Item("3rd item")
+///     }
+///     .headerSlideStyle(CustomizedHeaderSlideStyle())
+/// }
+/// ```
+///
+/// ## Topics
+/// ### Customize Style
+/// - ``HeaderSlideStyle``
+public struct HeaderSlide: Slide {
 
     @Environment(\.headerSlideStyle)
     private var style
 
     private var configuration: HeaderSlideStyleConfiguration
 
+    /// Creates a header slide.
+    /// - Parameters:
+    ///   - header: A text which is shown on the slide
+    ///   - fontWeight: A  fontWeight of the header text.
+    ///   - content: A content of the slide.
     public init(_ header: String, fontWeight: Font.Weight = .semibold, @ViewBuilder content: @escaping () -> some View) {
         self.configuration = HeaderSlideStyleConfiguration(
             header: .init {
