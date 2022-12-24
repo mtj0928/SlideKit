6476766f352f42bd81eec70dc6609802ed17a6cd
mtj0928 <mtj0928@users.noreply.github.com>
2022-12-24 17:20:45 +0900

    fix API

--- Sources/SlideKit/Views/Previews/SlidePreview.swift
+++ Sources/SlideKit/Views/Previews/SlidePreview.swift
@@ -13,6 +13,9 @@ public struct SlidePreview: View {
     @ObservedObject
     private var slideIndexController: SlideIndexController
 
+    @Environment(\.previewForegroundColor)
+    private var foregroundColor: Color
+
     @Environment(\.previewBackgroundColor)
     private var backgroundColor: Color
 
@@ -28,24 +31,36 @@ public struct SlidePreview: View {
             SlideRouterView(slideIndexController: slideIndexController)
                 .background(backgroundColor)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                .foregroundColor(.black)
+                .foregroundColor(foregroundColor)
         }
         .ignoresSafeArea()
         .aspectRatio(slideSize, contentMode: .fit)
     }
 
-    public func previewSlideBackgroundColor(_ color: Color) -> some View {
-        self.environment(\.previewBackgroundColor, color)
+    public func previewColor(foreground: Color = .black, background: Color = .white) -> some View {
+        self.environment(\.previewBackgroundColor, background)
+            .environment(\.previewForegroundColor, foreground)
     }
 }
 
-private enum Key: EnvironmentKey {
+private enum PreviewBackgroundColorKey: EnvironmentKey {
     static let defaultValue = Color.white
 }
 
 extension EnvironmentValues {
     fileprivate var previewBackgroundColor: Color {
-        get { self[Key.self] }
-        set { self[Key.self] = newValue }
+        get { self[PreviewBackgroundColorKey.self] }
+        set { self[PreviewBackgroundColorKey.self] = newValue }
+    }
+}
+
+private enum PreviewForegroundColorKey: EnvironmentKey {
+    static let defaultValue = Color.black
+}
+
+extension EnvironmentValues {
+    fileprivate var previewForegroundColor: Color {
+        get { self[PreviewForegroundColorKey.self] }
+        set { self[PreviewForegroundColorKey.self] = newValue }
     }
 }
