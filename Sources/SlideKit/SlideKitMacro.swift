e1fa0e9d088bc64cc76080779c9d304bdca25817
mtj0928 <mtj0928@users.noreply.github.com>
2023-08-09 07:24:11 +0900

    Update for Xcode 15 Beta 6

--- Sources/SlideKit/SlideKitMacro.swift
+++ Sources/SlideKit/SlideKitMacro.swift
@@ -1,3 +1,3 @@
-@attached(conformance)
+@attached(extension, conformances: Slide)
 @attached(member, names: named(_phase), named(_resolvePhaseStateStore), named(SlidePhasedState), named(phase), named(container), named(phasesStateSore), named(Phase))
 public macro Slide() = #externalMacro(module: "SlideKitMacros", type: "SlideMacro")
