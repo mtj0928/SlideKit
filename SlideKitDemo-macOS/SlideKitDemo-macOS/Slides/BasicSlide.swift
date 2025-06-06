432be02422e6d6eace3433b4941a1b7cd3b2eb8d
Kazuya Ueoka <info@fromkk.info>
2025-06-06 19:20:02 +0900

    fix scope
    Co-authored-by: matsuji <mtj0928@users.noreply.github.com>

--- SlideKitDemo-macOS/SlideKitDemo-macOS/Slides/BasicSlide.swift
+++ SlideKitDemo-macOS/SlideKitDemo-macOS/Slides/BasicSlide.swift
@@ -30,10 +30,10 @@ struct BasicSlide: View {
                 }
             }
         }
+    }
 
-        var transition: AnyTransition {
-            .scale
-        }
+    var transition: AnyTransition {
+        .scale
     }
 
     var script: String {
